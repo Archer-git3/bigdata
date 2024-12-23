e9bebd4026f4618808acff7740ec909be8cde210
Bogdankhavroniuk <60515937+Bogdankhavroniuk@users.noreply.github.com>
2024-12-23 17:25:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -188,4 +188,4 @@ def recommend():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=False, host = '0.0.0.0'  , port=100)
+    app.run(debug=False, host = '0.0.0.0'  , port=5000)
